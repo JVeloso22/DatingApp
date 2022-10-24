39cb62f70bcaf14cd42c86ba77aa9ebd99c652bf
JVeloso22 <jveloso@egapi.pt>
2022-10-24 12:13:24 +0100

    End of Section 8

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,37 @@
+using API.Extensions;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string Looking { get; set; }
+
+        public string Interests { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
+    }
+}
